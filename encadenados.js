e61d61878e21bd0cf0c993c7de628904bd789c02
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-07-15 16:08:34 -0600

    agregue cambios

--- encadenados.js
+++ encadenados.js
@@ -1,9 +1,20 @@
-const ciudadaDestino = 'sonzacate';
-const ciudadadesDisponibles = new Array("sonsonate", "sonzacate", "san salvador");
+const ciudadaDestino = 'san salvador';
+const ciudadadesDisponibles = new Array("sonsonate", "san salvador", "sonzacate");
+let edadPasajero = 17;
+let estaAcompañado = false;
 
-if ((ciudadadesDisponibles.indexOf(ciudadaDestino) >-1) && 
-    (edadPasajero >=18 || estaAcompañado)) {
-    console.log("pasaje disponible para venta");
-} else {
-    console.log("ciudad no disponible para venta o no cumple con los");
+// verificamos si la edad del pasajero cumple las reglas
+if (edadPasajero >=18 || estaAcompañado){
+    //evaluamo si la segunda rama esta disponible
+    if (ciudadadesDisponibles.indexOf(ciudadaDestino) >-1)  {
+        console.log("pasaje disponible para viajar");
+    } else {
+        console.log("ciudad no disponible para viajar");
+    }
+}else{
+    if (edadPasajero >= 16 && ciudadaDestino == 'san salvador'){
+        console.log("puedes viajar a san salvador");
+    }else{
+        console.log("pasajero no cumple las reglas");
+    }
 }
