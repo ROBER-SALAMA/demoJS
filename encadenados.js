079b39aabe22d19c74aa3f5406d6fd032d759a1c
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-07-15 15:47:41 -0600

    encadenado

--- encadenados.js
+++ encadenados.js
@@ -5,5 +5,5 @@ if ((ciudadadesDisponibles.indexOf(ciudadaDestino) >-1) &&
     (edadPasajero >=18 || estaAcompañado)) {
     console.log("pasaje disponible para venta");
 } else {
-    console.log("ciudad no disponible para venta o no cumple con los requisitos");
+    console.log("ciudad no disponible para venta o no cumple con los");
 }
